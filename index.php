62a584ec8026b15b6f27e10eed9bda67d8622f7a
ataev-timur <ataevtimur@gmail.com>
2015-05-30 12:45:55 +0300

    Change index file.

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <?php
 require 'vendor/autoload.php';
 
+use AtaevTimur\BinaryStudio\GameOfThrones\Crowlands\BaratheonOfKingsLanding\TommenBaratheon;
+use AtaevTimur\BinaryStudio\GameOfThrones\Dorne\Martell\DoranMartell;
+use AtaevTimur\BinaryStudio\GameOfThrones\Riverlands\Harrenhal\PetyrBaelish;
+use AtaevTimur\BinaryStudio\GameOfThrones\Stormlands\BaratheonOfStormsEnd\TommenI;
+use AtaevTimur\BinaryStudio\GameOfThrones\Westerlands\Lannister\CerseiLannister;
 use AtaevTimur\BinaryStudio\Mersenne\MersenneGenerator;
 
-foreach(MersenneGenerator::generate(10) as $value) {
-    echo $value . '<br>';
-}
+echo TommenBaratheon::whoami(), PHP_EOL;
+echo DoranMartell::whoami(), PHP_EOL;
+echo PetyrBaelish::whoami(), PHP_EOL;
+echo TommenI::whoami(), PHP_EOL;
+echo CerseiLannister::whoami(), PHP_EOL;
 
-echo \AtaevTimur\BinaryStudio\GameOfThrones\Crowlands\BaratheonOfKingsLanding\TommenBaratheon::whoami(), PHP_EOL;
-echo \AtaevTimur\BinaryStudio\GameOfThrones\Dorne\Martell\DoranMartell::whoami(), PHP_EOL;
-echo \AtaevTimur\BinaryStudio\GameOfThrones\Riverlands\Harrenhal\PetyrBaelish::whoami(), PHP_EOL;
-echo \AtaevTimur\BinaryStudio\GameOfThrones\Stormlands\BaratheonOfStormsEnd\TommenI::whoami(), PHP_EOL;
-echo \AtaevTimur\BinaryStudio\GameOfThrones\Westerlands\Lannister\CerseiLannister::whoami(), PHP_EOL;
+foreach(MersenneGenerator::generate(10) as $value) {
+    echo $value . ', ';
+}
